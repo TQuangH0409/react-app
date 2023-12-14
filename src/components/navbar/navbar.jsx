6f3bcf988f523705b2b9ddf172e604c151cfdbf9
TV-IT28935 <nguyentruongviet0305@gmail.com>
2023-12-14 20:00:28 +0700

    14-12

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,28 +1,61 @@
-import {
-  faBell,
-  faCommentDots,
-  faEnvelope,
-} from "@fortawesome/free-regular-svg-icons";
-import "./navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import img1 from "../../assets/images/avatar.jpg"
+import React from 'react'
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+import "./navbar.css"
 
-function Navbar({ title , avatar }) {
+const items = [
+  {
+    key: '1',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+        1st menu item
+      </a>
+    ),
+    
+  },
+  {
+    key: '2',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        2nd menu item
+      </a>
+    ),
+    
+  },
+  {
+    key: '3',
+    label: (
+      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+        3rd menu item 
+      </a>
+    ),
+    
+  },
+];
+
+const Navbar = () => {
   return (
-    <div className="v-navbar-container">
-      <div className="v-navbar-inner">
-        <h3 className="v-navbar-left">{title}</h3>
-        <div className="v-navbar-right">
-          <FontAwesomeIcon className="v-navbar-search" icon={faMagnifyingGlass} />
-          <FontAwesomeIcon className="v-navbar-mail" icon={faEnvelope} />
-          <FontAwesomeIcon className="v-navbar-bell" icon={faBell} />
-          <FontAwesomeIcon className="v-navbar-three-dots" icon={faCommentDots} />
-          <img src={img1}/>
-        </div>
-      </div> 
+    <div className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+      <ul className='navbar-nav ml-auto'>
+        <li className='navitem no-arrow'>
+          <Dropdown
+            menu={{
+              items,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+              <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                <img class="img-profile rounded-circle" src='../img/avatar.png'></img>
+              </Space>
+            </a>
+          </Dropdown>
+        </li>
+      </ul>
+
+
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
